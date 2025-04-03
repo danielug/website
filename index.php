742416f1103e46e8f1250f2fea77fb82f2e24de0
Daniel Luginbühl <daniel.luginbuehl@autoform.ch>
2025-04-03 18:31:43 +0200

    switch to php

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+	<meta charset="UTF-8">
+	<title>Daniel James</title>
+	<meta name="description" content="This site is currently under construction.">
+	<meta name="generator" content="hosting-page-builder">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="og:url" content="https://daniel-james.ch/index.html">
+	<meta name="og:locale" content="">
+	<meta name="og:type" content="website">
+	<meta name="og:title" content="daniel-james">
+	<meta name="og:description" content="Welcome to my website!">
+	<meta name="article:publisher" content="profile.php?id=61557169541826">
+	<meta name="og:updated_time" content="1741358096">
+	<link rel="stylesheet" href="styles/style.css">
+	<link rel="stylesheet" href="styles/style-home.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<header>
+	<div class="header-content-area">
+		<div class="flex-header">			
+			<a href="art.php" class="menu-item" id="menu-item-art">
+				<img class="menu-item-img" src="images/art.png">
+			</a>
+			<a href="index.php" class="menu-item" id="menu-item-home">
+				<img class="menu-item-img" src="images/home.png">
+			</a>
+			<a href="extra.php" class="menu-item" id="menu-item-extra">
+				<img class="menu-item-img" src="images/extra.png">
+			</a>
+		</div>
+	</div>		
+</header>
+
+<main>
+	<section class="intro">
+		<div class="flex-intro">
+			<div >
+				<img class="intro-image" src="images/me.JPG">
+			</div>
+			<div class="intro-text">
+				<h2 style="color: lightgoldenrodyellow">The blend of the artistic and technological aspects of Computer Graphics has always fascinated me. With my technical background, I especially enjoy applying my acquired knowledge to add functionality to a program and then visually judging the end results – be it for data visualization, simulations, or just a nicely rendered image.</h2>
+			</div>
+		</div>
+	</section>
+
+	<section class="biography">		
+			<div class="timeline-grid">
+				<div class="timeline-grid-header">
+					<h2>Education</h2>
+				</div>		
+				<div>
+					<h3>2018</h3>
+				</div>
+				<div>
+					<h3>ETH Zurich MSc Computer Science</h3>
+					<p>Visual Computing specialization track</p>					
+					<ul>
+						<li>Master’s Thesis -  VirtaMed: “Hexahedral based simulation in Projective Dynamics”, embedding of tetrahedra in a hexahedral mesh which is animated using the Projective Dynamics simulation technique</li>
+					</ul>
+				</div>
+
+				<div>
+					<h3>2015</h3>
+				</div>
+				<div>
+					<h3>ETH Zurich BSc Computer Science</h3>					
+					<ul>
+						<li>Bachelor's Thesis -  Disney Research Zurich: “Luminance Independent Chromaticity Preprocessing for HDR Video Coding”, a prototype to improve the chroma compression of the HEVC codec</li>
+					</ul>
+				</div>
+
+				<div>
+					<h3>2010</h3>
+				</div>
+				<div>
+					<h3>Harvard University</h3>
+					<p>Video Editing and Digital Design Course</p>
+				</div>
+
+				<div>
+					<h3>2009</h3>
+				</div>
+				<div>
+					<h3>Kantonsschule Zürcher Unterland</h3>
+					<p>Gymnasiale Maturität</p>
+				</div>
+			</div>		
+
+			<div class="timeline-grid">
+				<div class="timeline-grid-header">
+					<h2>Work Experience</h2>
+				</div>
+				<div>
+					<h3>2019<br>-<br>present</h3>
+				</div>
+				<div>
+					<h3>AutoForm Engineering GmbH</h3>
+					<p>Senior Software Engineer</p>
+					<ul>
+						<li>Extend and maintain the 3D rendering subsystem</li>
+						<li>Develop various tools and shaders in our 3D scene to visualize simulation results, reveal surface defects, perform measurements and improve the overall user experience</li>
+						<li>Geometry processing and postprocessing of sheet metal forming simulation results</li>
+					</ul>
+				</div>
+
+				<div>
+					<h3>2016<br>-<br>2017</h3>
+				</div>
+				<div>
+					<h3>ETH IT Software Services</h3>
+					<p>Testing and Quality Assurance</p>
+					<ul>
+						<li>Created and maintained scripts to automate regression tests for the WCMS of the ETH</li>
+						<li>Wrote automated tests to report broken links</li>
+					</ul>
+				</div>
+
+				<div>
+					<h3>2010</h3>
+				</div>
+				<div>
+					<h3>United Nations, New York City, USA</h3>
+					<p>Intern</p>
+					<ul>
+						<li>Created an opening slide show for Global Connections Television</li>
+						<li>Designed the DVD cover and poster for “Opening Doors”, a documentary about the General Assembly</li>
+					</ul>
+				</div>
+			</div>		
+	</section>
+
+	<section class="showcase">
+		<div class="showcase-flex">
+			<div class="showcase-entry">
+				<a href="https://www.autoform.com/en/" target="_blank">
+					<div>
+						<img class="showcase-entry-image" src="images/autoform.png">
+					</div>
+					<div class="showcase-entry-text">
+						<h2 class="showcase-entry-title">3D Graphics Team at AutoForm</h2>
+						<h3>Software Solutions for Sheet Metal Forming and BiW Assembly</h3>
+						<p>AutoForm’s software solutions form a comprehensive platform for the engineering, evaluation and improvement of the sheet metal forming and BiW assembly processes. This platform allows for full digitalization, seamless information and data flow, and integration of Industry 4.0 standards.</p>
+					</div>
+				</a>
+			</div>
+
+			<div class="showcase-entry">
+				<a href="https://buggygames.ch/" target="_blank">
+					<div>
+						<img class="showcase-entry-image" src="images/buggygames.png">
+					</div>
+					<div class="showcase-entry-text">
+						<h2 class="showcase-entry-title">Co-developer of Cowboy Mills</h2>
+						<h3>Nine Men's Morris Mobile Game</h3>
+						<p>Play the beloved board game of Mills, also known as Nine Men's Morris or Cowboy Checkers, with fancy 3D visuals. Create triplets and reduce your opponent's figure count to win the match. You can play against a friend or against a computer opponent. Do you think you measure up? Take the challenge, but as cowboys say: "Don't go in, if you don't know the way out!"</p>
+					</div>
+				</a>
+			</div>
+
+			<div class="showcase-entry">
+				<a href="https://patents.google.com/patent/US20160323556A1/en" target="_blank">
+					<div>
+						<img class="showcase-entry-image" src="images/patent.png">
+					</div>
+					<div class="showcase-entry-text">
+						<h2 class="showcase-entry-title">Patent Application Publication. Disney Enterprises, Inc., ETH Zurich </h2>
+						<h3>Perceptual color transformations for wide color gamut video coding</h3>
+						<p>A video coding pipeline is provided that can accommodate high dynamic range (HDR) and wide color gamut (WCG) content at a fixed bitrate. The video coding pipeline relies on separate chromaticity and luminance-specific transforms in order to process image content. Image content may be converted into a nearly perceptually uniform color space for coding in constant luminance. Moreover, chromaticity transforms are utilized which reduce coding errors in the chroma components (at the fixed bitrate) by enlarging the distribution of code words for compression.</p>
+					</div>
+				</a>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<div class="flex-footer">
+			<div>
+				<a href="https://linkedin.com/in/daniel-luginb%C3%BChl-a20257138" target="_blank">
+					<img src="images/linkedin-logo.svg" alt="Linked In" class="social-image">
+				</a>
+			</div>
+			<div>
+				<a href="https://www.facebook.com/profile.php?id=61557169541826" target="_blank">
+					<img src="images/facebook-logo.svg" alt="Facebook" class="social-image">
+				</a>
+			</div>
+			<div>
+				<a href="https://www.instagram.com/daniel.james1990" target="_blank">
+					<img src="images/instagram-logo.svg" alt="Instagram" class="social-image">
+				</a>
+			</div>
+		</div>
+	</footer>
+</main>
+
+	
+</body>
+</html>
